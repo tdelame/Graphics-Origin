d7321fec80c9f118d73b7267e547540e84ed2cf5
T. Delame <tdelame@gmail.com>
2016-04-16 22:36:12 +0100

    start to port the code to Windows

--- lib/tools/tools.cc
+++ lib/tools/tools.cc
@@ -8,6 +8,10 @@
 # include <string>
 # include <boost/locale.hpp>
 
+# ifdef _WIN32
+#   include <windows.h>
+# endif
+
 BEGIN_GO_NAMESPACE namespace tools {
 
   std::unordered_map < std::type_index, std::string >& get_type_to_name()
@@ -22,37 +26,78 @@ BEGIN_GO_NAMESPACE namespace tools {
     return name_to_serializer;
   }
 
+# ifdef __linux__ 
   static void initialize_library() __attribute__((constructor));
   static void finalize_library() __attribute__((destructor));
 
   void initialize_library()
   {
-    static bool initialized = false;
-    if( initialized ) return;
+	  static bool initialized = false;
+	  if (initialized) return;
 
-    REGISTER_ANY_SERIALIZATION_TYPE(int, "int");
-    REGISTER_ANY_SERIALIZATION_TYPE(bool,"bool");
-    REGISTER_ANY_SERIALIZATION_TYPE(std::string,"string");
-    REGISTER_ANY_SERIALIZATION_TYPE(float,"float");
-    REGISTER_ANY_SERIALIZATION_TYPE(double,"double");
-    REGISTER_ANY_SERIALIZATION_TYPE(real,"real");
+	  REGISTER_ANY_SERIALIZATION_TYPE(int, "int");
+	  REGISTER_ANY_SERIALIZATION_TYPE(bool, "bool");
+	  REGISTER_ANY_SERIALIZATION_TYPE(std::string, "string");
+	  REGISTER_ANY_SERIALIZATION_TYPE(float, "float");
+	  REGISTER_ANY_SERIALIZATION_TYPE(double, "double");
+	  REGISTER_ANY_SERIALIZATION_TYPE(real, "real");
 
-    boost::locale::generator gen;
-    std::locale loc = gen("");
-    std::locale::global(loc);
-    std::wcout.imbue(loc);
-    std::ios_base::sync_with_stdio(false);
+	  boost::locale::generator gen;
+	  std::locale loc = gen("");
+	  std::locale::global(loc);
+	  std::wcout.imbue(loc);
+	  std::ios_base::sync_with_stdio(false);
 
-    initialized = true;
+	  initialized = true;
   }
 
 
 
   void finalize_library()
   {
-    for( auto pair : get_name_to_serializer() )
-      {
-        delete pair.second;
-      }
+	  for (auto pair : get_name_to_serializer())
+	  {
+		  delete pair.second;
+	  }
   }
+# elif _WIN32
+  BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+  {
+	  if (fdwReason == DLL_PROCESS_ATTACH)
+	  {
+		  // equivalent of __attribute__((constructor))...
+		  REGISTER_ANY_SERIALIZATION_TYPE(int, "int");
+		  REGISTER_ANY_SERIALIZATION_TYPE(bool, "bool");
+		  REGISTER_ANY_SERIALIZATION_TYPE(std::string, "string");
+		  REGISTER_ANY_SERIALIZATION_TYPE(float, "float");
+		  REGISTER_ANY_SERIALIZATION_TYPE(double, "double");
+		  REGISTER_ANY_SERIALIZATION_TYPE(real, "real");
+
+		  boost::locale::generator gen;
+		  std::locale loc = gen("");
+		  std::locale::global(loc);
+		  std::wcout.imbue(loc);
+		  std::ios_base::sync_with_stdio(false);
+		  // return TRUE if succeeded, FALSE if you failed to initialize properly
+		  return TRUE; 
+	  }
+	  else if (fdwReason == DLL_PROCESS_DETACH)
+	    {
+		  // equivalent of __attribute__((destructor))...
+		  for (auto pair : get_name_to_serializer())
+		    {
+			  delete pair.second;
+   		    }
+		  
+	    }
+
+	  // Return value is ignored when fdwReason isn't DLL_PROCESS_ATTACH, so we'll
+	  // just return TRUE.
+	  return TRUE;
+  }
+# else
+#  error Unkown C++ Compiler
+# endif
+
+
 } END_GO_NAMESPACE
