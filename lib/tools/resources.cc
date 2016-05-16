b3d2ce8f6357bd14bb159f57e5490bfb678b46ad
T. Delame <tdelame@gmail.com>
2016-05-16 19:47:12 +0200

    start to work on resources management.

---
+++ lib/tools/resources.cc
@@ -0,0 +1,53 @@
+/*  Created on: May 16, 2016
+ *      Author: T. Delame (tdelame@gmail.com) */
+# include "../../graphics-origin/tools/resources.h"
+# include <boost/filesystem.hpp>
+BEGIN_GO_NAMESPACE
+  namespace tools
+  {
+    std::string
+    path_manager::get_root_directory () const noexcept
+    {
+      return m_root_directory;
+    }
+
+    void
+    path_manager::set_root_directory( const std::string& root ) noexcept
+    {
+      m_root_directory = root;
+    }
+
+    std::string
+    path_manager::get_resource_directory( const std::string& resource_name ) const noexcept
+    {
+      auto find = m_paths.find( resource_name );
+      if( find == m_paths.end() )
+        return m_root_directory + resource_name;
+      return find->second;
+    }
+
+    void
+    path_manager::set_relative_resource_directory( const std::string& resource_name ) noexcept
+    {
+      m_paths.erase( resource_name );
+    }
+
+    void
+    path_manager::set_absolute_resource_path(
+        const std::string& resource_name,
+        const std::string& resource_path ) noexcept
+    {
+      m_paths[resource_name] = resource_path;
+    }
+
+
+    path_manager& get_path_manager()
+    {
+      static path_manager instance;
+      return instance;
+    }
+
+
+  }
+END_GO_NAMESPACE
+
