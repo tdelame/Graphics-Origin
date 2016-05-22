46a60f03f5da43b1d561268a8f1f593ec5c7aa56
T. Delame <tdelame@gmail.com>
2016-05-22 17:14:00 +0200

    make more glm::mat4 transforms available to graphics_origin::mat4

--- graphics-origin/geometry/matrix.h
+++ graphics-origin/geometry/matrix.h
@@ -8,6 +8,7 @@
 
 # include <glm/glm.hpp>
 # include <glm/gtc/matrix_transform.hpp>
+# include <glm/gtx/transform.hpp>
 # include <glm/gtc/type_ptr.hpp>
 # include <glm/gtc/matrix_access.hpp>
 BEGIN_GO_NAMESPACE
